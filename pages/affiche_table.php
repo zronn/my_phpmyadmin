2c648901587827ac32a6e52be59c4a3c81655de8
Ronen-dev <ronenn.ma@gmail.com>
2015-05-28 15:14:23 +0200

    Fichier modifié

--- pages/affiche_table.php
+++ pages/affiche_table.php
@@ -2,6 +2,7 @@
   session_start();
   require_once '../check_session/connect_mysql.php';
   require_once '../check_session/needAuth.php';
+  mysqli_select_db($connect, $_GET['db']);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -142,55 +143,27 @@
                             <table class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
-                                        <th>Page</th>
-                                        <th>Visits</th>
-                                        <th>% New Visits</th>
-                                        <th>Revenue</th>
+                                        <th>Tables</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>/index.html</td>
-                                        <td>1265</td>
-                                        <td>32.3%</td>
-                                        <td>$321.33</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/about.html</td>
-                                        <td>261</td>
-                                        <td>33.3%</td>
-                                        <td>$234.12</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/sales.html</td>
-                                        <td>665</td>
-                                        <td>21.3%</td>
-                                        <td>$16.34</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/blog.html</td>
-                                        <td>9516</td>
-                                        <td>89.3%</td>
-                                        <td>$1644.43</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/404.html</td>
-                                        <td>23</td>
-                                        <td>34.3%</td>
-                                        <td>$23.52</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/services.html</td>
-                                        <td>421</td>
-                                        <td>60.3%</td>
-                                        <td>$724.32</td>
-                                    </tr>
-                                    <tr>
-                                        <td>/blog/post.html</td>
-                                        <td>1233</td>
-                                        <td>93.2%</td>
-                                        <td>$126.34</td>
-                                    </tr>
+                                    <?php
+                                        if (!empty($connect))
+                                        {
+                                          $query = "show tables";
+                                          $result = mysqli_query($connect, $query);
+                                          if (!$result)
+                                          {
+                                            echo "ERROR";
+                                          }
+                                          while ($data = mysqli_fetch_array($result))
+                                          {
+                                            echo "<tr>";
+                                            echo "<td><a href='affiche_contenu.php?db=" . $data[0] . "'>" . $data[0] ."</a></td>";
+                                            echo "</tr>";
+                                          }  
+                                        }
+                                    ?>
                                 </tbody>
                             </table>
                         </div>
