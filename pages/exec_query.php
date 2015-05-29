51b615de9166e5f713b5de2fb46b2b09b9148feb
belhadef.fr@gmail.com <belhadef.fr@gmail.com>
2015-05-29 17:21:45 +0200

    sohw result of query

--- pages/exec_query.php
+++ pages/exec_query.php
@@ -6,20 +6,48 @@ if ((isset($_POST['db'])) && (!empty($_POST['db'])))
 	mysqli_select_db($connect, $_POST['db']);
 if ((isset($_POST['query'])) && (!empty($_POST['query'])))
 {
-	$query;
-	$sql = $_POST['query'];
-	$msc = microtime(true);
-	$query = mysqli_query($connect, $sql);
-	$msc = microtime(true)-$msc;
-	$time = ($msc * 1000) . ' ms';
-	if (!$query)
+
+	$ex = explode(" ", $query);
+
+	if (strtolower($ex[0]) != "select")
 	{
-		$error = mysqli_error($connect);
-		$reponse = array('result' => "SQL error : $error");
+		$query;
+		$sql = $_POST['query'];
+		$msc = microtime(true);
+		$query = mysqli_query($connect, $sql);
+		$msc = microtime(true)-$msc;
+		$time = ($msc * 1000) . ' ms';
+		if (!$query)
+		{
+			$error = mysqli_error($connect);
+			$reponse = array('result' => "SQL error : $error");
+		}
+		else
+			$reponse = array('result' => "Requette Sql Ok -> Traitement en $time");
 	}
 	else
-		$reponse = array('result' => "Requette Sql Ok -> Traitement en $time");
+	{
+		$query;
+		$sql = $_POST['query'];
+		$msc = microtime(true);
+		$query = mysqli_query($connect, $sql);
+		$msc = microtime(true)-$msc;
+		$time = ($msc * 1000) . ' ms';
+		if (!$query)
+		{
+			$error = mysqli_error($connect);
+			$reponse = array('result' => "SQL error : $error");
+		}
+		else
+			$reponse = array('result' => "Requette Sql Ok -> Traitement en $time");
 
+	}
+
+
+
+
+
+	
 	echo json_encode($reponse);
 
 }	
