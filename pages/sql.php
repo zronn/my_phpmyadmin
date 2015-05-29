f4d7777d755bc6f29b3a8c1c2f96c759f03b98ac
belhadef.fr@gmail.com <belhadef.fr@gmail.com>
2015-05-29 10:47:32 +0200

    sql.php

--- pages/sql.php
+++ pages/sql.php
@@ -2,20 +2,19 @@
 session_start();
 require_once '../check_session/connect_mysql.php';
 require_once '../check_session/needAuth.php';
-$db = $_GET['db'];
+if(isset($_GET['db']))
+    $db = $_GET['db'];
 mysqli_select_db($connect, $db);
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="phpmyadmin">
+    <meta name="author" content="belhad_b mazouz_r gentie_r etna">
 
     <title>My_PhpMyAdmin</title>
 
@@ -37,7 +36,29 @@ mysqli_select_db($connect, $db);
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
         <![endif]-->
+        <script type="text/javascript" src="../js/jquery.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
 
+                $("#submit").click(function(){
+                    var query = $("#query").val();
+                    //console.log($("#query").val());
+                    $.ajax({
+                            url: "exec_query.php",
+                            type: "POST", 
+                            data: {query : query},
+                            dataType : "json", 
+                            success: function(data) { 
+                                alert(data); 
+                            },
+                            error: function(data) {
+                                alert(data);
+                            }
+                    });
+                });
+
+            });
+        </script>
     </head>
 
     <body>
@@ -45,116 +66,70 @@ mysqli_select_db($connect, $db);
         <div id="wrapper">
 
             <!-- Navigation -->
-            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="index.html">My_PhpMyAdmin</a>
-                </div>
-                <!-- Top Menu Items -->
-                <ul class="nav navbar-right top-nav">
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $_SESSION['user'];?> <b class="caret"></b></a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-user"></i> Mon profil</a>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-fw fa-gear"></i> Paramètres</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <a href="deconnexion.php"><i class="fa fa-fw fa-power-off"></i> Déconnexion</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav side-nav">
-                        <li class="active">
-                            <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="export.php"><i class="fa fa-fw fa-send"></i> Exporter</a>
-                        </li>
-                        <li>
-                            <a href="import.php"><i class="fa fa-fw fa-shield"></i> Importer</a>
-                        </li>
-                        <li>
-                            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
-                        </li>
-                        <li>
-                            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Databases <i class="fa fa-fw fa-caret-down"></i></a>
-                            <ul id="demo" class="collapse">
-                                <?php
-                                if (isset($connect))
-                                {
-                                  $query = "show databases";
-                                  $result = mysqli_query($connect, $query);
-                                  if (!$result)
-                                  {
-                                    echo "ERROR";
-                                }
-                                while ($data = mysqli_fetch_row($result))
-                                {
-                                    echo "<li>";
-                                    echo "<a href='affiche_table.php?db=" . $data[0] . "'>" . $data[0] ."</a>";
-                                    echo "</li>";
-                                }  
-                            }
-                            ?>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </nav>
+            <?php
+            include("navig.php");
+            ?>
 
-        <div id="page-wrapper">
-            <div class="container-fluid">
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            SQL <small>Code</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li class="active">
-                                <i class="fa fa-code"></i> Saisir du code sql dans la fenetre ci-dessous :
-                            </li>
-                        </ol>
+            <div id="page-wrapper">
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h1 class="page-header">
+                                SQL <small>Code</small>
+                            </h1>
+                            <ol class="breadcrumb">
+                                <li class="active">
+                                    Votre requette seras executer sur la base de donnees : <?php echo strtoupper($db); ?>
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-1">
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h3 class="panel-title"><i class="fa fa-keyboard-o fa-fw"></i> Saisir votre requette sql dans la fenetre ci-dessous :</h3>
+                                </div>
+                                <div class="panel-body">
+                                    <form role="form">
+                                        <div class="form-group">
+                                            <label class="pull-right" for="comment"><i class="fa fa-exclamation-triangle"></i> Respecter la Syntaxe Sql.</label>
+                                            <br>
+                                            <textarea class="form-control" rows="7" id="query"></textarea>
+                                        </div>
+
+                                        <br>
+                                    </form>
+                                    <div class="text-center">
+                                        <button class="btn btn-success" id="submit">Executer</button>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-1">
+                        </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-12">
-
-                    </div>
-                </div>
-
             </div>
+            <!-- /#page-wrapper -->
+
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /#wrapper -->
 
-    </div>
-    <!-- /#wrapper -->
+        <!-- jQuery -->
+        <script src="../js/jquery.js"></script>
 
-    <!-- jQuery -->
-    <script src="../js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../js/bootstrap.min.js"></script>
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+        <!-- Morris Charts JavaScript -->
+        <script src="../js/plugins/morris/raphael.min.js"></script>
+        <script src="../js/plugins/morris/morris.min.js"></script>
+        <script src="../js/plugins/morris/morris-data.js"></script>
+    </body>
 
-    <!-- Morris Charts JavaScript -->
-    <script src="../js/plugins/morris/raphael.min.js"></script>
-    <script src="../js/plugins/morris/morris.min.js"></script>
-    <script src="../js/plugins/morris/morris-data.js"></script>
-
-</body>
-
-</html>
+    </html>
